1b9b606caaf4e76a52b847abf0221c46bfb67ab4
Lex006 <106839300+Lex006@users.noreply.github.com>
2022-09-04 13:11:03 -0300

    Add files via upload

---
+++ MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+var ball;
+
+var ball_options={
+	isStatic:false,
+	restituition:0.3,
+	friction:0
+	density:1.2
+}
+
+    groundObj.display();
+
+	groundObj = new ground(width/2,670,width,20);
+	leftSide = new ground(1100,600,20,120);
+
+	Engine.run(engine);
+  
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(Ball,)
+	}
+}
+
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
